3e33e4edb7d17e261baab3a25177e07a2d5206e8
alex02r <alex.giochi02@gmail.com>
2023-12-21 14:23:48 +0100

    [BONUS]Creazione del contenuto di AppHero

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -4,8 +4,10 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-        
+    <div style="border: 1px solid black">
+        <h1>Jumbotron</h1>
+        <h3>Questo è VUE</h3>
+        <img src="../assets/vue.svg" alt="Logo">
     </div>
 </template>
 <style lang="">
